791f9c87a5f0e3dd9ab3e6c4bf5d3341e0b602ea
supercat1337 <albert.bazaleev@gmail.com>
2025-07-09 18:48:00 +0300

    Update examples

--- device-detect/example/main.js
+++ device-detect/example/main.js
@@ -20,7 +20,7 @@ const {
     getBrowserLanguage,
     isMac,
 } = await import(
-    "https://cdn.jsdelivr.net/npm/@supercat1337/device-detect@1.0.2/dist/device-detect.esm.min.js?" +
+    "https://cdn.jsdelivr.net/npm/@supercat1337/device-detect@1.0.3/dist/device-detect.esm.min.js?" +
         new Date().getTime()
 );
 
